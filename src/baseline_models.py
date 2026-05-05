18773ada2a9f02baae892c0e4da5f417e8c1f9fd
Md. Robiul Islam Niloy <37466356+Mdniloykhan@users.noreply.github.com>
2026-05-05 14:01:03 +0600

    Update baseline_models.py

--- src/baseline_models.py
+++ src/baseline_models.py
@@ -16,7 +16,7 @@ import os
 # ==============================
 # 1. Load Dataset
 # ==============================
-df = pd.read_csv("data/oss_threat_dataset_beta")
+df = pd.read_csv("data/oss_threat_dataset_beta.csv")
 
 # Combine title + description
 df["text"] = df["title"] + " " + df["description"]
